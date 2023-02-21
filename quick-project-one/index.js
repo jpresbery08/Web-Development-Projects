af82c7ba3c987959b5e58fa7671e9f1c2df62202
jpresbery08 <jpresbery25@gmail.com>
2023-02-20 18:14:05 -0800

    Modified mouse scrolling animation heavily

--- quick-project-one/index.js
+++ quick-project-one/index.js
@@ -4,18 +4,26 @@ const track = document.getElementById("image-track");
 // Storing value of where exact mouse click is placed and will update the mouse clicked location
 window.onmousedown = e => {
     track.dataset.mouseDownAt = clientX;
+
+}
+// Setting orignal value back to zero once mouse is released
+window.onmouseup = e => {
+    track.dataset.mouseDownAt = "0"; 
 }
 
 // Function that takes updated mouse location minus the orginal mouse position
 window.onmousemove = e => {
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+    // This wiill return the value (stopping the code from progressing) so that the track doesnt move if the mouse HASNT been clicked
+    if(track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX; 
     const maxDelta = window.innerWidth/2; // Max width of screen with 50% space on both sides
 
     // current screen position / max screen width in decimal form (Ex. 43/100)
-    const percentage = (mouseDelta / maxDelta) * 100;
+    const percentage = (mouseDelta / maxDelta) * -100;
 
     // starts from 0% on the screen and can flucate with wherever the mouse is clicked at and store in the perecentage area
-    track.style.transform = `translate(${percentage}%, 0%)`;
+    track.style.transform = `translate(${percentage}%, -50%)`;
 }
 
 
